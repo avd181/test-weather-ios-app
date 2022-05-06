584af8d50aebc08c9a391367268c017bee61aae5
Nasty <>
2022-05-06 21:18:35 +0400

    initial commit

---
+++ test-weather-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-weather-ios-app
+//
+//  Created by Nasty on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
